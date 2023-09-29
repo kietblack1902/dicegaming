cf6cd457b9fccbd4f9ad544e9ff9f9bbc3d17d39
kietblack1902 <kietblack1902@gmail.com>
2023-09-29 11:38:02 +0700

    project game

---
+++ index.js
@@ -0,0 +1,29 @@
+function roll() {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+    var randomDiceImage = "xucxac" + randomNumber1 + ".png";
+
+    var randomImageSource = "images/" + randomDiceImage;
+
+    var image1 = document.querySelectorAll("img")[0];
+
+    image1.setAttribute("src", randomImageSource);
+
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+    var randomImageSource2 = "images/xucxac" + randomNumber2 + ".png";
+
+    document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+    if (randomNumber1 > randomNumber2) {
+      document.querySelector("h1").innerHTML = "Người chơi 1 chiến thắng!";
+    }
+    else if (randomNumber2 > randomNumber1) {
+      document.querySelector("h1").innerHTML = "Người chơi 2 chiến thắng!";
+    }
+    else {
+      document.querySelector("h1").innerHTML = "Hoà rồi nè <3!";
+    }
+
+}
+document.getElementById("roll").onclick = function() { roll(); };
